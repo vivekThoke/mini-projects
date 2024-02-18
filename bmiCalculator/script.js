6e417a4389bae5f18dac60e94c019607c98ac343
vivekThoke <vivekthoke5@gmail.com>
2024-02-18 21:50:47 +0530

    second project

---
+++ bmiCalculator/script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector("form");
+
+
+form.addEventListener("click", function(e){
+    e.preventDefault()
+
+    const hieght = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#wieght").value);
+    const result = document.querySelector(".results");
+
+    if (hieght === "" || hieght < 0 || isNaN(hieght)){
+        result.innerHTML = `Enter the valid Details ${hieght}`
+    }else if (weight === "" || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Enter the valid details ${weight}`
+    } else {
+        const bmi = (weight / ((hieght * hieght )/ 10000)).toFixed(2)
+
+        result.innerHTML = `<span>Your BMI is ${bmi}</span>`
+
+        if (bmi < 18.6) {
+            result.innerHTML = `<span>Your are Under weight ${bmi}</span>`
+        }
+        else if(bmi >18.6 && bmi < 24.9) {
+            result.innerHTML = `<span>Your are Normal Weight ${bmi}</span>`
+        }
+        else {
+            result.innerHTML = `<span>Your are Over Weight ${bmi}</span>`
+        }
+
+    }
+
+})
